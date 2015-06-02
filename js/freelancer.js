9dd77e258acde29a9f4a279c219b9042c3fac84b
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-06-01 18:40:25 -0600

    Works well with Jekyll. Pretty well to go.

--- js/freelancer.js
+++ js/freelancer.js
@@ -6,7 +6,7 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
+    $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
