729784c3fe8c16e8963881446552c73414a384c4
Kepler Sticka-Jones <kepler+git@stickajones.org>
2018-07-06 00:24:06 -0600

    Make Progressive Web App

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,16 @@
+const siteMetadata = {
+  title: "AtomLinter",
+  url: "https://atomlinter.github.io",
+  description: "Lint your code with ease in Atom.",
+  color: {
+    primary: "#efeae1",
+    secondary: "#66595c",
+    active: "#81c08b"
+  }
+};
+
 module.exports = {
-  siteMetadata: {
-    title: "AtomLinter",
-    url: "https://atomlinter.github.io",
-    description: "Lint your code with ease in Atom.",
-    color: {
-      primary: "#efeae1",
-      secondary: "#66595c",
-      active: "#81c08b"
-    }
-  },
+  siteMetadata,
   plugins: [
     "gatsby-plugin-lodash",
     "gatsby-transformer-yaml",
@@ -19,6 +21,19 @@ module.exports = {
       }
     },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-typescript"
+    "gatsby-plugin-typescript",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.title,
+        start_url: "/",
+        background_color: siteMetadata.color.secondary,
+        theme_color: siteMetadata.color.primary,
+        display: "minimal-ui",
+        icon: "src/images/logo.svg" // This path is relative to the root of the site.
+      }
+    },
+    "gatsby-plugin-offline"
   ]
 };
