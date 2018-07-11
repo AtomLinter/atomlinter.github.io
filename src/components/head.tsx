82d9c0be960a534844e9e01cd4b99eb48cb97699
Kepler Sticka-Jones <kepler+git@stickajones.org>
2018-07-11 10:20:48 -0600

    Remove some old and Gatsby-handled elements from the head

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -22,17 +22,8 @@ const Head = () => (
     `}
     render={data => (
       <Helmet defaultTitle={data.site.siteMetadata.title}>
-        <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width" />
         <meta name="description" content={data.site.siteMetadata.description} />
         <link rel="canonical" href={data.site.siteMetadata.url} />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
 
         <link
           rel="stylesheet"
