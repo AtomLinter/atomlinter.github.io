d19ce1271a50f2e4878ac7538566317f50a1187f
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-06 17:14:35 -0600

    Transition to statuc query hook from HOC

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,24 @@
-import { graphql, StaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 
-const Footer = () => (
-  <StaticQuery
-    query={graphql`
-      query FooterQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={data => (
-      <footer className="mdl-mini-footer">
-        <div className="mdl-mini-footer__left-section">
-          <div className="mdl-logo">{data.site.siteMetadata.title}</div>
-        </div>
-      </footer>
-    )}
-  />
-);
+    }
+  `);
+
+  return (
+    <footer className="mdl-mini-footer">
+      <div className="mdl-mini-footer__left-section">
+        <div className="mdl-logo">{data.site.siteMetadata.title}</div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,64 +1,66 @@
-import { graphql, StaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import { sortBy } from "lodash";
 import * as React from "react";
 
-const Providers = () => (
-  <StaticQuery
-    query={graphql`
-      query ProvidersQuery {
-        providers: allProvidersYaml {
-          edges {
-            node {
-              id
+const Providers = () => {
+  const data = useStaticQuery(graphql`
+    query ProvidersQuery {
+      providers: allProvidersYaml {
+        edges {
+          node {
+            id
+            title
+            plural
+            modal
+            types {
               title
-              plural
               modal
-              types {
+              packages {
                 title
-                modal
-                packages {
-                  title
-                  url
-                }
+                url
               }
             }
           }
         }
       }
-    `}
-    render={data => (
-      <>
-        {data.providers.edges.map(({ node: scope }: any) => (
-          <div key={scope.id} id={scope.modal}>
-            <h2>{scope.title}</h2>
-            <div className="mdl-grid">
-              {sortBy(scope.types, "title").map((type: any) => (
-                <div
-                  className="mdl-card mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-shadow--2dp"
-                  key={`${scope.modal}-${type.modal}`}
-                  id={`${scope.modal}-${type.modal}`}
-                >
-                  <div className="mdl-card__title">
-                    <h2 className="mdl-card__title-text">{type.title}</h2>
-                  </div>
-                  {sortBy(type.packages, "title").map((pkg: any) => (
-                    <div key={`${scope.modal}_${type.modal}_${pkg.title}`} className="mdl-card__actions mdl-card--border">
-                      <a
-                        className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-                        href={pkg.url}
-                      >
-                        {pkg.title}
-                      </a>
-                    </div>
-                  ))}
+    }
+  `);
+
+  return (
+    <>
+      {data.providers.edges.map(({ node: scope }: any) => (
+        <div key={scope.id} id={scope.modal}>
+          <h2>{scope.title}</h2>
+          <div className="mdl-grid">
+            {sortBy(scope.types, "title").map((type: any) => (
+              <div
+                className="mdl-card mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-shadow--2dp"
+                key={`${scope.modal}-${type.modal}`}
+                id={`${scope.modal}-${type.modal}`}
+              >
+                <div className="mdl-card__title">
+                  <h2 className="mdl-card__title-text">{type.title}</h2>
                 </div>
-              ))}
-            </div>
+                {sortBy(type.packages, "title").map((pkg: any) => (
+                  <div
+                    key={`${scope.modal}_${type.modal}_${pkg.title}`}
+                    className="mdl-card__actions mdl-card--border"
+                  >
+                    <a
+                      className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+                      href={pkg.url}
+                    >
+                      {pkg.title}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
-        ))}
-      </>
-    )}
-  />
-);
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Providers;
